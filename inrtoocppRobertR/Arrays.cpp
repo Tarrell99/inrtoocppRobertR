b1c50c33e960e55b071cd91807310af9dd98c6a7
tarrellriley <tarrellriley@gmail.com>
2018-08-27 17:14:43 -0500

    class work

---
+++ inrtoocppRobertR/Arrays.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+int main()
+{
+	char fritsname[] = { 'M', 'A', 'T', 'T', 'H', 'E', 'W' };
+	int  number[7];
+	for (int i = 0; i < 7; i++)
+	{
+		std::cout << number[i];
+		number[i] = (int)fritsname[i];
+	}
+
+	{
+		int Mynumber[] = { 12 , 21, 3, 7, 12 };
+
+		for (int i = 0; i < 5; i++);
+	}
+	{
+		int input[10];
+		for (int i = 0; i < 20; i++)
+		{
+			input[i] % 2;
+
+		}
+	}
+
+	{
+		int input[9];
+		for (int i)
+
+	}
+
+	return 0;
+}
