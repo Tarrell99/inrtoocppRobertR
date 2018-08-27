3d3578eef7c7f75c393629d0dc3042d80a0aa1b7
tarrellriley <tarrellriley@gmail.com>
2018-08-27 17:52:17 -0500

    clas work

--- inrtoocppRobertR/Arrays.cpp
+++ inrtoocppRobertR/Arrays.cpp
@@ -29,5 +29,29 @@ int main()
 
 	}
 
+	//problem 1
+	{
+		/*int numbers[10] = { 0, 0, 1, 0, 0, 1, 0, 0, 1, 1 };
+		int matrix[5] = { 1, 2, 3, 4, 5, 6, 7 };
+		double radii[10] = (3.2, 4.7};
+	int table[7] = { 2, , , 27, , 45, 39 };
+	char codes[] = { 'A', 'X', '1', '2', 's' };
+	int blanks[];
+	int collection[-20];
+	int hours[3] = 8, 12, 16;*/
+	}
+	//problem 2
+	{
+
+	}
+	//problem 3
+	{
+
+	}
+	//problem 4
+	{
+
+	}
+	//problem 5
 	return 0;
 }
