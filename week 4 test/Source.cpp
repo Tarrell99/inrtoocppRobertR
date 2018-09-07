f0525b8a290dfd2edb822e0b2e8fe677084e6639
tarrellriley <tarrellriley@gmail.com>
2018-09-07 12:31:24 -0500

    home work

---
+++ week 4 test/Source.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+
+void odd(int arr, int size)
+{
+	//print !%2 
+	int temp;
+	for (int i = size ; i < arr, i !%2)
+	{
+		std::cout << i << std::endl;
+	}
+
+}
+
+int main()
+{
+	int Myarry[6];
+
+	system("pause");
+}
