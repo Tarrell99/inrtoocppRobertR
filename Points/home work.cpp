717d33b3a900a4005d93f3a8b56d00cc694a54a5
tarrellriley <tarrellriley@gmail.com>
2018-09-12 17:50:04 -0500

    home work

---
+++ Points/home work.cpp
@@ -0,0 +1,11 @@
+//Write a function for each of the following descriptions.For each function, use the pointer
+//notation ONLY.Do NOT use the array index[] notation.
+//A.Write a function RevString(char* array) which reverses array.The function
+//returns nothing.
+//B.Write a function CountEven(int* array, int array_len) which receives an
+//integer array and its size, and returns the number of even numbers in the array.
+//C.Write a function Maximum(double* array, int array_size) that returns a
+//pointer to the maximum value of an array of doubles.If the array is empty, return
+//nullptr.
+//D.Write a function Contains(char* array, char search_value) which returns
+//true if the 1st parameter contains the 2nd parameter, or false otherwise.
