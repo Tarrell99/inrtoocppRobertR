29c1054d81f3695c0c4ceb20368020fa656df23a
tarrellriley <tarrellriley@gmail.com>
2018-09-16 19:44:06 -0500

    was not able to get #3 to work it keeps returnibg a invid number

--- reference/Source.cpp
+++ reference/Source.cpp
@@ -1,20 +1,67 @@
 #include<iostream>
 
-int main()
+struct MyStruct
 {
-	int variable = 10;
-	std::cout << variable << std::endl;
+	int rhs = 2;
+	int lhs = 2;
+
+};
+
+void changeIt(int rhs, int lhs)
+{
+	rhs = 2;
+	lhs = 1;
+
+		 
+}
+
+
+int main()
+//#1
+{
+//	int variable = 10;
+//	std::cout << variable << std::endl;
+//	system("pause");
+//
+//
+//	int &myVariable = variable;
+//	std::cout << myVariable << std::endl;
+//	/*Change the contents of the variable by directly changing the variable and using the
+//		reference*/
+//	system("pause");
+//
+//
+//	variable = 12;
+//	std::cout << myVariable << std::endl;
+//	system("pause");
+//
+//
+//	/*Create another reference to the same integer and prove to yourself that both
+//		references are referring to the same int.*///	int &myVariable2 = variable;
+//	std::cout << myVariable2 << std::endl;
+//
+//
+//	//Try creating a reference which doesn’t refer to
+//	/*anything and try to make an existing reference refer to something else.
+//		Also try making a reference to an int refer to a float. 2*/
+//	int num = 5;
+//	int &myVariable = num;
+//	/*int &myRef = */
+//	float F = 4.2;
+//	/*int &myVariable2 = F;*/
+//	//cant be done
+//	system("pause");
+
+
+
+	//#2
+
+	std::cout << changeIt << std::endl;
+
 	system("pause");
-	int &myVariable = variable;
-	std::cout << myVariable << std::endl;
-	/*Change the contents of the variable by directly changing the variable and using the
-		reference*/
-	system("pause");
-	variable = 12;
-	std::cout << myVariable << std::endl;
-	system("pause");
-	/*Create another reference to the same integer and prove to yourself that both
-		references are referring to the same int.*/	int &myVariable2 = variable;
-	std::cout << myVariable2 << std::endl;
-	system("pause");
+
+
+
+	//#3
+
 }
