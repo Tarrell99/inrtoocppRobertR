17085d2353d1773df08a126e490f19e596abfda2
tarrellriley <tarrellriley@gmail.com>
2018-09-19 07:49:41 -0500

    woked on it
    you can do it dont give up

---
+++ callstack/ReadMe.txt
@@ -0,0 +1,11 @@
+This is my CallStack assignment in it i attempted to make a calculator that would allow 
+
+you to push on different numbers and when ever you added an addition or subtraction sign 
+
+it would then take off the top two numbers and would do the operation you had pushed on 
+
+to the stack.
+
+Unfortunately i was not able to get it to work, the functions i have i believe are on the 
+
+right track to working but i was not able to get it there.
--- callstack/callstack.cpp
+++ callstack/callstack.cpp
@@ -7,20 +7,32 @@ private:
 	int count = 0; // index
 	
 public:
-	bool isEmpty()
-	{
-		for(isEmpty > 1)
-	}
 	
+	// tells you what is on top
 	int top()
-	{ 
-
-
+	{
+		// it should point to the last number that was pushed on but i do not know if i 
+		// need the {count = count + 1} to make sure it was the last one pushed.
+		int* myTop = &values[0];
+		/*count = count + 1;*/
 	}
-
+	//takes an item off the top of the stack
 	void pop()
 	{
-
+		values[count] = -1;
+		count = count - 1;
+		if (int i = 42)
+		{
+			// it would then add 
+		}
+		else if (int j = 45)
+		{
+			// it would then mins
+		}
+		else
+		{
+			return ;
+		}
 	}
 
 	void push(int val)
@@ -29,8 +41,20 @@ public:
 		//add an item to the top of the stack
 		values[count] = val;
 		count = count + 1;
+		std::cin >> val;
 	}
-
+	// tells if the next pop will go below 1
+	bool isEmpty()
+	{
+		// my line of thinking was if myTop points to something less then one it would
+		// come back as fales but i see the problm with it now but im not really sure how 
+		// to fix it.
+		while (int* myTop = &values[1])
+		{
+			return 0;
+		}
+		
+	}
 };
 
 
@@ -39,6 +63,8 @@ int main()
 {
 	Stack Stack;
 	Stack.push(20);
+	
+	
 }
 
 
