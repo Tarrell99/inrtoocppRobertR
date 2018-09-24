a492932505f2b50cc8c297a3316830451b096516
tarrellriley <tarrellriley@gmail.com>
2018-09-24 07:21:41 -0500

    Not working?

--- labOO/Game.cpp
+++ labOO/Game.cpp
@@ -3,21 +3,27 @@
 
 Hero Game::Battle(Hero &fighter1, Hero &fighter2)
 {
-	/*if (fighter1 == fighter2)
+	if (fighter1 == fighter2)
 	{
 
-	}*/
+	}
+	std::cout << "IronMan" << fighter1.getHealth << std::endl;
+	std::cout << "CaptainAmerica" << fighter2.getHealth << std::endl;
 	while (fighter1.isAlive() && fighter2.isAlive())
 	{
 		fighter1.Fight(fighter2);
 		fighter2.Fight(fighter1);
+		std::cout << "IronMan" << fighter1.getHealth << std::endl;
+		std::cout << "CaptainAmerica" << fighter2.getHealth << std::endl;
 	}
 	if (fighter1.isAlive())
 	{
+		std::cout << "IronMan won" << std::endl;
 		return fighter1;
 	}
 	else if (fighter2.isAlive())
 	{
+		std::cout << "CaptainAmerica won" << std::endl;
 		return fighter2;
 	}
 
--- labOO/Hero.h
+++ labOO/Hero.h
@@ -10,8 +10,10 @@ public:
 	void Fight(Hero&);
 	bool isAlive();
 	void TakeDamage(int);
-	//bool operator == (Hero & other)
-	//{
-	//	return m_Health == other.m_Health && m_Health == other.m_Health;
-	//}
+	bool operator == (Hero & other)
+	{
+		return m_Health == other.m_Health && m_Health == other.m_Health;
+	}
+	int* getHealth = &m_Health;
+	int* getPower = &m_Power;
 };
