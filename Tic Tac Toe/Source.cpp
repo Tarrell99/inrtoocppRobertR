b44dbdf53feb71a9cef9e7cb4b7be7d6301754f8
tarrellriley <tarrellriley@gmail.com>
2018-10-01 17:58:10 -0500

    try

---
+++ Tic Tac Toe/Source.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main()
+{ // the board
+	char input = '+';
+
+	char gamePiece;
+	char board[3][3] = { { '| |', '| |', '| |', },{ '| |', '| |', '| |', },{ '| |', '| |', '| |', } };
+	char board[3][3] = { { ' ', ' ', ' ' },{ ' ', ' ', ' ' },{ ' ', ' ', ' ' } };
+	int choicePlace = 0;
+	int X = 0;
+	int O = 0;
+	system("pause");
+}
