cdfae2180ef586335e4c62a585899e01fed70bc6
tarrellriley <tarrellriley@gmail.com>
2018-10-04 23:40:58 -0500

    done

--- Tic Tac Toe/Source.cpp
+++ Tic Tac Toe/Source.cpp
@@ -10,6 +10,7 @@ int main()
 	
 	std::cout << "This is the Tic Tac Toe Game to play all you need to do is use the number pad as a borad for  player2" << std::endl;
 	std::cout << " Player 1 will use 'q,w,e,a,s,d,z,x,c' as the pad " << std::endl;
+	std::cout << " If you try to place a piece that all ready has one it will bypass your turn so try not to or it may be game over " << std::endl;
 	system("pause");
 	system("cls");
 
@@ -19,6 +20,7 @@ int main()
 		if (i %2 == 0)
 		{
 			system("cls");
+			std::cout << "its " << gamePiece2 << " Turn" << std::endl;
 			std::cout << "|" << board[0][0] << "|" << board[0][1] << "|" << board[0][2] << "|" << std::endl;
 			std::cout << "|" << board[1][0] << "|" << board[1][1] << "|" << board[1][2] << "|" << std::endl;
 			std::cout << "|" << board[2][0] << "|" << board[2][1] << "|" << board[2][2] << "|" << std::endl;
@@ -28,210 +30,227 @@ int main()
 
 			if (placementNum == 9)
 			{
-				board[0][2] = gamePiece2;
-			
+				
+				if (board[0][2] == ' ')
+				{
+					board[0][2] = gamePiece2;
+				}
 			}
 			if (placementNum == 8)
 			{
-				board[0][1] = gamePiece2;
+				
+				if (board[0][1] == ' ')
+				{
+					board[0][1] = gamePiece2;
+				}
 			}
 			if (placementNum == 7)
 			{
-				board[0][0] = gamePiece2;
+				
+				if (board[0][0] == ' ')
+				{
+					board[0][0] = gamePiece2;
+				}
 			}
 			if (placementNum == 6)
 			{
-				board[1][2] = gamePiece2;
+				
+				if (board[1][2] == ' ')
+				{
+					board[1][2] = gamePiece2;
+				}
 			}
 			if (placementNum == 5)
 			{
-				board[1][1] = gamePiece2;
+				
+				if (board[1][1] == ' ')
+				{
+					board[1][1] = gamePiece2;
+				}
 			}
 			if (placementNum == 4)
 			{
-				board[1][0] = gamePiece2;
+				
+				if (board[1][0] == ' ')
+				{
+					board[1][0] = gamePiece2;
+				}
 			}
 			if (placementNum == 3)
 			{
-				board[2][2] = gamePiece2;
+				
+				if (board[2][2] == ' ')
+				{
+					board[2][2] = gamePiece2;
+				}
 			}
 			if (placementNum == 2)
 			{
-				board[2][1] = gamePiece2;
+				
+				if (board[2][1] == ' ')
+				{
+					board[2][1] = gamePiece2;
+				}
 			}
 			if (placementNum == 1)
 			{
-				board[2][0] = gamePiece2;
+				
+				if (board[2][0] == ' ')
+				{
+					board[2][0] = gamePiece2;
+				}
 			}
+			
 			system("cls");
 			std::cout << "|" << board[0][0] << "|" << board[0][1] << "|" << board[0][2] << "|" << std::endl;
 			std::cout << "|" << board[1][0] << "|" << board[1][1] << "|" << board[1][2] << "|" << std::endl;
 			std::cout << "|" << board[2][0] << "|" << board[2][1] << "|" << board[2][2] << "|" << std::endl;
-			
-			// Top row win
-			if (board[0][0] == board[0][1] && board[0][2] == board[0][1])
-			{
-				std::cout << board[0][0] << " Won " << std::endl;
-				break;
-			}
-			// Middle row win
-			else if (board[1][0] == board[1][1] && board[1][2] == board[1][1])
-			{
-				std::cout << board[1][0] << " Won " << std::endl;
-				break;
-			}
-			// bottion row win
-			else if (board[2][0] == board[2][1] && board[2][2] == board[2][1])
-			{
-				std::cout << board[2][0] << " Won " << std::endl;
-				break;
-			}
-			// Middle collom win
-			else if (board[0][1] == board[1][1] && board[2][1] == board[1][1])
-			{
-				std::cout << board[0][1] << " Won " << std::endl;
-				break;
-			}
-			// Right collom win
-			else if (board[0][2] == board[1][2] && board[2][2] == board[1][2])
-			{
-				std::cout << board[0][2] << " Won " << std::endl;
-				break;
-			}
-			// left \ win
-			else if (board[0][0] == board[1][1] && board[2][2] == board[1][1])
-			{
-				std::cout << board[0][0] << " Won " << std::endl;
-				break;
-			}
-			// left collom win
-			else if (board[0][0] == board[1][0] && board[2][0] == board[1][0])
-			{
-				std::cout << board[0][0] << " Won " << std::endl;
-				break;
-			}
-			// Right / win 
-			else if (board[0][2] == board[1][1] && board[2][0] == board[1][1])
-			{
-				std::cout << board[0][2] << " Won " << std::endl;
-				break;
-			}
-			else
-			{
-				std::cout << " Looks like the Cat won it was a tie" << std::endl;
-				break;
-			}
-
 		}
 
 
 		if (i %2 == 1 )
 		{
+			
 			system("cls");
+			std::cout << "its " << gamePiece << " Turn" << std::endl;
 			std::cout << "|" << board[0][0] << "|" << board[0][1] << "|" << board[0][2] << "|" << std::endl;
 			std::cout << "|" << board[1][0] << "|" << board[1][1] << "|" << board[1][2] << "|" << std::endl;
 			std::cout << "|" << board[2][0] << "|" << board[2][1] << "|" << board[2][2] << "|" << std::endl;
 			std::cin >> placementCha;
 
-			if (placementCha == 'e')
+			if (placementCha == 'e' || placementCha == 'E')
 			{
-				board[0][2] = gamePiece;
+				
+				if (board[0][2] == ' ')
+				{
+					board[0][2] = gamePiece;
+				}
 			}
-			if (placementCha == 'w')
+			if (placementCha == 'w' || placementCha == 'W')
 			{
-				board[0][1] = gamePiece;
+				
+				if (board[0][1] == ' ')
+				{
+					board[0][1] = gamePiece;
+				}
 			}
-			if (placementCha == 'q')
+			if (placementCha == 'q'|| placementCha == 'Q')
 			{
-				board[0][0] = gamePiece;
+				
+				if (board[0][0] == ' ')
+				{
+					board[0][0] = gamePiece;
+				}
 			}
-			if (placementCha == 'd')
+			if (placementCha == 'd' || placementCha == 'D')
 			{
-				board[1][2] = gamePiece;
+				
+				if (board[1][2] == ' ')
+				{
+					board[1][2] = gamePiece;
+				}
 			}
-			if (placementCha == 's')
+			if (placementCha == 's' || placementCha == 'S')
 			{
-				board[1][1] = gamePiece;
+				
+				if (board[1][1] == ' ')
+				{
+					board[1][1] = gamePiece;
+				}
 			}
-			if (placementCha == 'a')
+			if (placementCha == 'a' || placementCha == 'A')
 			{
-				board[1][0] = gamePiece;
+				
+				if (board[1][0] == ' ')
+				{
+					board[1][0] = gamePiece;
+				}
 			}
-			if (placementCha == 'c')
+			if (placementCha == 'c' || placementCha == 'C')
 			{
-				board[2][2] = gamePiece;
+			
+				if (board[2][2] == ' ')
+				{
+					board[2][2] = gamePiece;
+				}
 			}
-			if (placementCha == 'x')
+			if (placementCha == 'x' || placementCha == 'X')
 			{
-				board[2][1] = gamePiece;
+			
+				if (board[2][1] == ' ')
+				{
+					board[2][1] = gamePiece;
+				}
 			}
-			if (placementCha == 'z')
+			if (placementCha == 'z' || placementCha == 'Z')
 			{
-				board[2][0] = gamePiece;
+				
+				if (board[2][0] == ' ')
+				{
+					board[2][0] = gamePiece;
+				}
 			}
+		
 		}
 		system("cls");
 		std::cout << "|" << board[0][0] << "|" << board[0][1] << "|" << board[0][2] << "|" << std::endl;
 		std::cout << "|" << board[1][0] << "|" << board[1][1] << "|" << board[1][2] << "|" << std::endl;
 		std::cout << "|" << board[2][0] << "|" << board[2][1] << "|" << board[2][2] << "|" << std::endl; 
 		
+		
+
+
+
 		// Top row win
-		if (board[0][0] == board[0][1] && board[0][2] == board[0][1])
-		{
-			std::cout << board[0][0] << " Won " << std::endl;
-			break;
-		}
+			if (board[0][0] == board[0][1] && board[0][2] == board[0][1] && (board[0][1] == gamePiece || board[0][1] == gamePiece2))
+			{
+				std::cout << board[0][0] << " Won " << std::endl;
+				break;
+			}
 		// Middle row win
-		else if (board[1][0] == board[1][1] && board[1][2] == board[1][1])
-		{
-			std::cout << board[1][0] << " Won " << std::endl;
-			break;
-		}
+			else if (board[1][0] == board[1][1] && board[1][2] == board[1][1] && (board[1][1] == gamePiece || board[1][1] == gamePiece2))
+			{
+				std::cout << board[1][0] << " Won " << std::endl;
+				break;
+			}
 		// bottion row win
-		else if (board[2][0] == board[2][1] && board[2][2] == board[2][1])
-		{
-			std::cout << board[2][0] << " Won " << std::endl;
-			break;
-		}
+			else if (board[2][0] == board[2][1] && board[2][2] == board[2][1] && (board[2][1] == gamePiece || board[2][1] == gamePiece2))
+			{
+				std::cout << board[2][0] << " Won " << std::endl;
+				break;
+			}
 		// Middle collom win
-		else if (board[0][1] == board[1][1] && board[2][1] == board[1][1])
-		{
-			std::cout << board[0][1] << " Won " << std::endl;
-			break;
-		}
+			else if (board[0][1] == board[1][1] && board[2][1] == board[1][1] && (board[1][1] == gamePiece || board[1][1] == gamePiece2))
+			{
+				std::cout << board[0][1] << " Won " << std::endl;
+				break;
+			}
 		// Right collom win
-		else if (board[0][2] == board[1][2] && board[2][2] == board[1][2])
-		{
-			std::cout << board[0][2] << " Won " << std::endl;
-			break;
-		}
+			else if (board[0][2] == board[1][2] && board[2][2] == board[1][2] && (board[1][2] == gamePiece || board[1][2] == gamePiece2))
+			{
+				std::cout << board[0][2] << " Won " << std::endl;
+				break;
+			}
 		// left \ win
-		else if (board[0][0] == board[1][1] && board[2][2] == board[1][1])
-		{
-			std::cout << board[0][0] << " Won " << std::endl;
-			break;
-		}
+			else if (board[0][0] == board[1][1] && board[2][2] == board[1][1] && (board[1][1] == gamePiece || board[1][1] == gamePiece2))
+			{
+				std::cout << board[0][0] << " Won " << std::endl;
+				break;
+			}
 		// left collom win
-		else if (board[0][0] == board[1][0] && board[2][0] == board[1][0])
-		{
-			std::cout << board[0][0] << " Won " << std::endl;
-			break;
-		}
+			else if (board[0][0] == board[1][0] && board[2][0] == board[1][0] && (board[1][0] == gamePiece || board[1][0] == gamePiece2))
+			{
+				std::cout << board[0][0] << " Won " << std::endl;
+				break;
+			}
 		// Right / win 
-		else if (board[0][2] == board[1][1] && board[2][0] == board[1][1])
-		{
-			std::cout << board[0][2] << " Won " << std::endl;
-			break;
-		}
-		else
-		{
-			std::cout << " Looks like the Cat won it was a tie" << std::endl;
-			break;
-		}
+			else if (board[0][2] == board[1][1] && board[2][0] == board[1][1] && (board[1][1] == gamePiece || board[1][1] == gamePiece2))
+			{
+				std::cout << board[0][2] << " Won " << std::endl;
+				break;
+			}
+
 	}
-	
-	
-
-
+	std::cout << "Game Over";
 	system("pause"); 
 }
