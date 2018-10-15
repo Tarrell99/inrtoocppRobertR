1de0ef04b28f2caa54c799914e996d7e24ea4db9
tarrellriley <tarrellriley@gmail.com>
2018-10-15 10:04:49 -0500

    mr

---
+++ week5/morging prog.cpp
@@ -0,0 +1,23 @@
+//#include<iostream>
+////Create a function that takes in a character pointer. The function will return the character 
+////that appears the most in the data the character pointer is pointing to.
+////
+////Test Case :
+////char *data = "Apple";
+////char common = MostCommon(data);
+////Most common value in the word apple should be the character p.
+//
+//char comValues(char* data , char MostCommonValue)
+//{
+//	char* currnt;
+//	char* nCurrent;
+//	char* pCurrent;
+//	currnt = data++;
+//	return MostCommonValue;
+//}
+//
+//int main()
+//{
+//	char MyData[10] = { 'a', 'p','p','l','e' };
+//	char* data = MyData;
+//}
